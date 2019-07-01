85d6f377174612a54b7b2fb3661f12916764e585
EC2 Default User <ec2-user@ip-172-31-38-195.ec2.internal>
2019-07-01 10:22:42 +0000

    update of user details & Home Page layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create!(email: "e@test.com", first_name: "Emily", surname: "Bowe", password: "secretpw", password_confirmation: "secretpw", is_doctor: true)
+User.create!(email: "richard@mail.com", first_name: "Richard", surname: "Condon", password: "secretpw", password_confirmation: "secretpw", is_doctor: true)
+User.create!(email: "paul@mail.com", first_name: "Paul", surname: "Boland", password: "secretpw", password_confirmation: "secretpw", is_doctor: true)
+User.create!(email: "mark@mail.com", first_name: "Mark", surname: "Sparky", password: "secretpw", password_confirmation: "secretpw", is_doctor: true)
 
 (1..5).each do |number|
   User.create!(email: "#{number}@test.com", first_name: "#{number}", surname: "#{number}son", password: "secretpw", password_confirmation: "secretpw", is_doctor: [true, false].sample)
 end
+
 
